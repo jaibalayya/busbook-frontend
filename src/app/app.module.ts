2969287e97ddd3a55268c9f65687a551a4b80c83
chinu <chinu@gmail.com>
2018-05-23 15:44:22 +0530

    components routed main component and admin component profile and login component and signup compnnenet is added status : working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './main/home/home.component';
+import { FooterComponent } from './main/footer/footer.component';
+import { HeaderComponent } from './main/header/header.component';
+import { MainComponent } from './main/main.component';
+import { AdminComponent } from './admin/admin.component';
+import {ProfileComponent} from './main/home/profile/profile.component';
+import {LoginComponent} from './main/home/profile/login/login.component';
+import {SignupComponent} from './main/home/profile/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainComponent,
+    AdminComponent,
+    ProfileComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
